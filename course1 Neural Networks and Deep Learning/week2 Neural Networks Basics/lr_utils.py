d73a75d2234ac553abff18d853e67b507cb4e77a
mushan65535 <474456031@qq.com>
2018-09-08 09:44:02 +0800

    Update lr_utils.py

--- course1 Neural Networks and Deep Learning/week2 Neural Networks Basics/lr_utils.py
+++ course1 Neural Networks and Deep Learning/week2 Neural Networks Basics/lr_utils.py
@@ -1,37 +1,19 @@
-import cv2 
-import tensorflow as tf
 import numpy as np
-import json
-import matplotlib.pyplot as plt
-
-
-with open('/home/long0420/文档/学习上的玩意/ai.challenger/ai_challenger_keypoint_train_20170902/keypoint_train_annotations_20170909.json', 'r') as f:
-    data = json.load(f)
+import h5py
     
+    
+def load_dataset():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-max_human = 0
-max_shape = np.zeros([3,1])
-for i in range(len(data)):
-    max_human = max(max_human, len(data[i]['human_annotations']))
-print(max_shape)
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
-pic_dic = '/home/long0420/文档/学习上的玩意/ai.challenger/ai_challenger_keypoint_train_20170902/keypoint_train_images_20170902/'
-pic_name = pic_dic + str(data[0]['image_id'])+ '.jpg'
-
-temp = cv2.imread (pic_name)
-cv2.imshow('ss',temp)
-flag = temp.shape
-plt.imshow(temp)
-flag2 = cv2.resize(temp,(800*800*3,1))
-print(flag2)
-
-
-i = 0
-max_length = 0
-max_width = 0
-for dat in data:
-    i = i + 1
-    print(i)
-    t = cv2.imread(pic_dic + str(dat['image_id']) + '.jpg')
-    max_length = max(max_length, t.shape[0])
-    max_width = max(max_width, t.shape[1])
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
